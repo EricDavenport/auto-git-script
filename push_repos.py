9fcc719e8d8648a0267701033a58304ac2d7ca30
Wayne <wayne.folkes@gmail.com>
2024-10-03 15:43:49 -0400

    Update push_repos.py

--- push_repos.py
+++ push_repos.py
@@ -1,30 +1,35 @@
 import os
 from git import Repo
+import logging
 from datetime import datetime
 
+# Set up logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
+
 # Path to repo directory
-REPOS_DIR = os.path.expanduser('/Users/eric_dav/Desktop/repos')
+REPOS_DIR = os.path.expanduser('~/Desktop/repos')
 
 def push_repos(repo_path):
     try:
         repo = Repo(repo_path)
-        # Check if the repo has a remote set up
-        if repo.remotes:
+        # Check if the repo has a remote set up and the repo has uncommitted changes
+        if repo.remotes and repo.is_dirty():
             # Add all changes, commit and push
             repo.git.add(A=True)
             repo.index.commit(f"Auto-commit on {datetime.now().strftime(f'%a %b-%d-%y %H:%M %p')}")
             repo.remotes.origin.push()
-            print(f'Successfully pushed {repo_path}')
+            log.info(f'Successfully pushed {repo_path}')
         else:
-            prit(f'No remote found for {repo_path}')
+            log.info(f'No remote found for {repo_path}')
     except Exception as e:
-        print(f'Failed to push {repo_path}: {e}')
+        log.error(f'Failed to push {repo_path}: {e}')
 
 def find_and_push_repos(root_dir):
-    for dirpath, dirnames, filenames in os.walk(root_dir):
+    for dirpath, __, _ in os.walk(root_dir):
     # Check if the current directory is a Git repo
         if os.path.exists(os.path.join(dirpath, '.git')):
-            print(f'Pushing repo: {dirpath}')
+            log.info(f'Pushing repo: {dirpath}')
             push_repos(dirpath)
 
 if __name__ == "__main__":
